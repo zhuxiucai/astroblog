b4caf21073bf7ea11fe8494a1cb3cf5c2a4bc656
zhuxiucai <zhuxiucai@126.com>
2025-06-06 01:20:31 +0800

    names modified

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shufan Blog';
+export const SITE_DESCRIPTION = 'Hosted on Github Pages';

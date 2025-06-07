37290ad60e1d50fdb5f1e432399c68bce61cfce8
zhuxiucai <zhuxiucai@126.com>
2025-06-08 01:54:16 +0800

    explicit config ghost secret

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -3,6 +3,6 @@ import GhostContentAPI from '@tryghost/content-api';
 // 使用站点凭证创建 API 实例
 export const ghostClient = new GhostContentAPI({
     url: 'http://pmosred.alotus.com.cn:80', // 这是 Ghost 站点运行在本地环境中的默认 URL
-    key: import.meta.env.CONTENT_API_KEY,
+    key: 549f4e7f7a0c08bc7ed68ff3e0,
     version: 'v5.0',
 });
